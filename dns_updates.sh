f1f556cdaf3babce86053ce0d0284bfd59df78be
madhu ganta <madhu@madhus-MacBook-Pro.local>
2016-12-12 09:11:02 -0600

    moving to cent 7

--- dns_updates.sh
+++ dns_updates.sh
@@ -1,51 +1,57 @@
 #!/bin/sh
-# cat a here-doc represenation of the hooks to the appropriate file
-cat > /etc/dhcp/dhclient-exit-hooks <<"EOF"
+# RHEL 7.2 uses NetworkManager. Add a script to be automatically invoked when interface comes up.
+cat > /etc/NetworkManager/dispatcher.d/12-register-dns <<"EOF"
 #!/bin/bash
-printf "\ndhclient-exit-hooks running...\n\treason:%s\n\tinterface:%s\n" "${reason:?}" "${interface:?}"
+# NetworkManager Dispatch script
+# Deployed by Cloudera Director Bootstrap
+#
+# Expected arguments:
+#    $1 - interface
+#    $2 - action
+#
+# See for info: http://linux.die.net/man/8/networkmanager
+
+# Register A and PTR records when interface comes up
 # only execute on the primary nic
-if [ "$interface" != "eth0" ]
+if [ "$1" != "eth0" || "$2" != "up" ]
 then
     exit 0;
 fi
+
 # when we have a new IP, perform nsupdate
-if [ "$reason" = BOUND ] || [ "$reason" = RENEW ] ||
-   [ "$reason" = REBIND ] || [ "$reason" = REBOOT ]
-then
-    printf "\tnew_ip_address:%s\n" "${new_ip_address:?}"
-    host=$(hostname | cut -d'.' -f1)
-    domain=$(hostname | cut -d'.' -f2- -s)
-    domain=${domain:='cdh-util.internal'} # If no hostname is provided, use cdh-util.internal
-    if [[ $(hostname -s) = j* ]]; then
+new_ip_address="$DHCP4_IP_ADDRESS"
+
+host=$(hostname -s)
+domain=$(hostname | cut -d'.' -f2- -s)
+domain=${domain:='cdh-util.internal'} # REPLACE-ME If no hostname is provided, use cdh-cluster.internal
+if [[ $(hostname -s) = j* ]]; then
        domain=cdh-jump.internal
-    elif [[ $(hostname -s) = w* ]]; then
+elif [[ $(hostname -s) = w* ]]; then
        domain=cdh-worker.internal
-    elif [[ $(hostname -s) = m* ]]; then
+elif [[ $(hostname -s) = m* ]]; then
        domain=cdh-master.internal
-    elif [[ $(hostname -s) = d*  ]]; then
+elif [[ $(hostname -s) = d*  ]]; then
        domain=cdh-util.internal
-    fi
-    IFS='.' read -ra ipparts <<< "$new_ip_address"
-    ptrrec="${ipparts[3]}.${ipparts[2]}.${ipparts[1]}.${ipparts[0]}.in-addr.arpa"
-    nsupdatecmds=$(mktemp -t nsupdate.XXXXXXXXXX)
-    resolvconfupdate=$(mktemp -t resolvconfupdate.XXXXXXXXXX)
-    echo updating resolv.conf
-    grep -iv "search" /etc/resolv.conf > "$resolvconfupdate"
-    echo "search cdh-worker.internal cdh-master.internal cdh-jump.internal cdh-util.internal" >> "$resolvconfupdate"
-    cat "$resolvconfupdate" > /etc/resolv.conf
-    echo "Attempting to register $host.$domain and $ptrrec"
-    {
-        echo "update delete $host.$domain a"
-        echo "update add $host.$domain 600 a $new_ip_address"
-        echo "send"
-        echo "update delete $ptrrec ptr"
-        echo "update add $ptrrec 600 ptr $host.$domain"
-        echo "send"
-    } > "$nsupdatecmds"
-    nsupdate "$nsupdatecmds"
 fi
-#done
+IFS='.' read -ra ipparts <<< "$new_ip_address"
+ptrrec="$(printf %s "$new_ip_address." | tac -s.)in-addr.arpa"
+nsupdatecmds=$(mktemp -t nsupdate.XXXXXXXXXX)
+resolvconfupdate=$(mktemp -t resolvconfupdate.XXXXXXXXXX)
+echo updating resolv.conf
+grep -iv "search" /etc/resolv.conf > "$resolvconfupdate"
+echo "search $domain" >> "$resolvconfupdate"
+cat "$resolvconfupdate" > /etc/resolv.conf
+echo "Attempting to register $host.$domain and $ptrrec"
+{
+    echo "update delete $host.$domain a"
+    echo "update add $host.$domain 600 a $new_ip_address"
+    echo "send"
+    echo "update delete $ptrrec ptr"
+    echo "update add $ptrrec 600 ptr $host.$domain"
+    echo "send"
+} > "$nsupdatecmds"
+nsupdate "$nsupdatecmds"
 exit 0;
 EOF
-chmod 755 /etc/dhcp/dhclient-exit-hooks
+chmod 755 /etc/NetworkManager/dispatcher.d/12-register-dns
 service network restart
